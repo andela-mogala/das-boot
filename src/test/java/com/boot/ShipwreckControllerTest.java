faab94eee99460980be9cb2007e67ebb4cb6334a
Michael Ogala <michael.ogala@andela.com>
2017-07-30 02:01:49 +0100

    add controller, repository and integration tests

--- src/test/java/com/boot/ShipwreckControllerTest.java
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -1,13 +1,18 @@
 package com.boot;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import static org.mockito.Mockito.*;
 
 import com.boot.controller.ShipwreckController;
 import com.boot.model.Shipwreck;
@@ -32,6 +37,64 @@ public class ShipwreckControllerTest {
 		when(shipwreckRepository.findOne(1l)).thenReturn(sw);
 		
 		Shipwreck wreck = sc.get(1l);
-		assertEquals(1L, wreck.getId().longValue());
+//		assertEquals(1L, wreck.getId().longValue());
+		assertThat(wreck.getId(), is(1l));
+	}
+	
+//	@Test
+//	public void testShipwreckDelete() {
+//		Shipwreck wreck = new Shipwreck();
+//		wreck.setId(1l);
+//		
+//		when(shipwreckRepository.findOne(1l)).thenReturn(wreck);
+//		when(shipwreckRepository.delete(wreck)).thenReturn();
+//		
+//		assertEquals(sc.delete(1l), wreck);
+//	}
+	
+	@Test
+	public void testShipwreckList() {
+		List<Shipwreck> wrecks = new ArrayList<Shipwreck>();
+		wrecks.add(new Shipwreck());
+		wrecks.add(new Shipwreck());
+		wrecks.add(new Shipwreck());
+		
+		when(shipwreckRepository.findAll()).thenReturn(wrecks);
+		
+		assertEquals(wrecks, sc.list());
+	}
+	
+	@Test
+	public void testShipwreckCreate() {
+		Shipwreck wreck = new Shipwreck();
+		wreck.setId(2l);
+		wreck.setName("Atlantis");
+		
+		when(shipwreckRepository.saveAndFlush(wreck)).thenReturn(wreck);
+		
+		assertEquals(wreck, sc.create(wreck));
+		assertEquals(wreck.getName(), sc.create(wreck).getName());
+		assertEquals(wreck.getId(), sc.create(wreck).getId());
+	}
+	
+	@Test
+	public void testShipwreckUpdate() {
+		Shipwreck wreck = new Shipwreck();
+		wreck.setId(1l);
+		wreck.setName("Emperor");
+		
+		Shipwreck updateShipwreck = new Shipwreck();
+		updateShipwreck.setName("Enterprise");
+		updateShipwreck.setId(1l);
+		
+		when(shipwreckRepository.findOne(1l)).thenReturn(wreck);
+		when(shipwreckRepository.saveAndFlush(wreck)).thenReturn(updateShipwreck);
+		
+		Shipwreck sampleShipwreck = new Shipwreck();
+		sampleShipwreck.setName("Enterprise");
+		
+		Shipwreck response = sc.update(1l, sampleShipwreck);
+		assertEquals(response.getName(), updateShipwreck.getName());
+		assertEquals(response.getId(), updateShipwreck.getId());
 	}
 }
